86638c73ebb2e09cc38e4e5bb5cc4ebfb6fa78e6
JOSEALM3IDA <jfeaisec@gmail.com>
2021-04-02 05:26:08 +0100

    Added front end for operator order for Depth-First Search

--- src/main/java/jalmeida/pathfindingvisualizer/views/main/Drawer.java
+++ src/main/java/jalmeida/pathfindingvisualizer/views/main/Drawer.java
@@ -4,20 +4,26 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.Hr;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.CssImport;
 import jalmeida.pathfindingvisualizer.algorithms.Algorithm;
 import jalmeida.pathfindingvisualizer.algorithms.DepthFirst;
 import jalmeida.pathfindingvisualizer.views.pathfindingvisualizer.GridContainer;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 
 @JsModule("./shared-styles.js")
 @CssImport("./drawer.css")
@@ -27,6 +33,7 @@ public class Drawer extends AppLayout {
     private static final String DEPTH_FIRST = "Depth-First Search";
     private static final String A_STAR = "A*";
 
+    private Label optionsLabel;
     private TextField colsField;
     private TextField rowsField;
     private Button resetObstaclesButton;
@@ -36,6 +43,10 @@ public class Drawer extends AppLayout {
     private Button clearButton;
     private Checkbox obstacleCheckbox;
     private Select<String> algorithmSelect;
+
+    private Label extraLabel;
+    private Label operatorOrderLabel;
+    private ArrayList<IntegerField> operatorOrderFields;
 
     private final GridContainer gridContainer;
     private final Component drawerContent;
@@ -63,12 +74,16 @@ public class Drawer extends AppLayout {
         logoLayout.add(lsvLogo);
         logoLayout.add(new H1("Pathfinding Visualizer"));
 
+        optionsLabel = new Label("General Options");
+        optionsLabel.setClassName("title-label");
+
         HorizontalLayout inputLayout = getGridSizeInputLayout();
         VerticalLayout buttonLayout = getButtonLayout();
         HorizontalLayout checkboxLayout = getCheckboxLayout();
         VerticalLayout selectLayout = getSelectLayout();
+        VerticalLayout extraLayout = getExtraLayout();
 
-        layout.add(logoLayout, inputLayout, checkboxLayout, selectLayout, buttonLayout);
+        layout.add(logoLayout, optionsLabel, inputLayout, checkboxLayout, selectLayout, buttonLayout, extraLayout);
         return layout;
     }
 
@@ -103,6 +118,9 @@ public class Drawer extends AppLayout {
 
         startButton = new Button("Start");
         startButton.addClickListener(e -> {
+            if (!validateOptions())
+                return;
+
             clearButton.click();
             gridContainer.solve();
         });
@@ -182,6 +200,91 @@ public class Drawer extends AppLayout {
         return vLayout;
     }
 
+    private VerticalLayout getExtraLayout() {
+        VerticalLayout vLayout = new VerticalLayout();
+        vLayout.add(new Hr());
+
+        extraLabel = new Label("Extra Options");
+        extraLabel.addClassName("title-label");
+        vLayout.add(extraLabel);
+
+        operatorOrderLabel = new Label("Operator Order");
+        operatorOrderLabel.addClassName("subtitle-label");
+        vLayout.add(operatorOrderLabel);
+
+        operatorOrderFields = new ArrayList<IntegerField>(4);
+        operatorOrderFields.add(new IntegerField("Up"));
+        operatorOrderFields.add(new IntegerField("Down"));
+        operatorOrderFields.add(new IntegerField("Left"));
+        operatorOrderFields.add(new IntegerField("Right"));
+
+        operatorOrderFields.get(0).setValue(1);
+        operatorOrderFields.get(1).setValue(3);
+        operatorOrderFields.get(2).setValue(4);
+        operatorOrderFields.get(3).setValue(2);
+
+        for (IntegerField f : operatorOrderFields) {
+            f.setClassName("operator-num-field");
+            f.setMax(4);
+            f.setMin(1);
+            f.setHasControls(true);
+        }
+
+        HorizontalLayout hLayout = new HorizontalLayout();
+        hLayout.setClassName("operator-num-field-pair");
+        hLayout.add(operatorOrderFields.get(0));
+        hLayout.add(operatorOrderFields.get(1));
+        vLayout.add(hLayout);
+
+
+        HorizontalLayout hLayout2 = new HorizontalLayout();
+        hLayout2.setClassName("operator-num-field-pair");
+        hLayout2.add(operatorOrderFields.get(2));
+        hLayout2.add(operatorOrderFields.get(3));
+        vLayout.add(hLayout2);
+
+        return vLayout;
+    }
+
+    private boolean validateOptions() { return handleOperatorOrderValidity(); }
+
+    private boolean handleOperatorOrderValidity() {
+        boolean isValid = true;
+        HashSet<IntegerField> invalidFields = new HashSet<>();
+
+        for (IntegerField intf : operatorOrderFields)
+            intf.setInvalid(false);
+
+        for (IntegerField intf : operatorOrderFields)
+            if (!isValidOperatorOrderField(intf)) {
+                isValid = false;
+                invalidFields.add(intf);
+            }
+
+        for (IntegerField intf : invalidFields)
+            intf.setInvalid(true);
+
+        if (!isValid)
+            throwErrorNotification("Operator order must be unique!");
+
+        return isValid;
+    }
+
+    private boolean isValidOperatorOrderField(IntegerField intf) {
+        int value = intf.getValue();
+        boolean isInvalid = false;
+        for (IntegerField field : operatorOrderFields) {
+            if (field == intf)
+                continue;
+
+            if (value == field.getValue())
+                isInvalid = true;
+
+        }
+
+        return !isInvalid;
+    }
+
     private void onInputFieldChange(TextField field, String name) {
         try {
             int value = Integer.parseInt(field.getValue());
@@ -233,6 +336,7 @@ public class Drawer extends AppLayout {
 
     private void throwErrorNotification(String msg) {
         Notification notification = new Notification(msg);
+        notification.setPosition(Notification.Position.BOTTOM_END);
         notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
         notification.setDuration(8000);
         notification.open();
